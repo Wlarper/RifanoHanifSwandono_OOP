b18fd0ff622a30fd89367fba80832e1cfdc62f51
Rifanosss <uncalledfornow@gmail.com>
2026-09-04 11:11:13 +0700

    RifanoHanifSwandono -init CS1-The thingy thing attack die die

--- Frontend_RifanoHanifSwandono/core/src/main/java/com/Rifano/frontend/Enemy.java
+++ Frontend_RifanoHanifSwandono/core/src/main/java/com/Rifano/frontend/Enemy.java
@@ -10,4 +10,22 @@ public class Enemy {
         this.hp = hp;
         this.maxHp = hp;
     }
+    public void takeDamage(int damage) {
+        this.hp -= damage;
+        if (this.hp < 0) {
+            this.hp = 0;
+        }
+        System.out.println(this.name + " took" + damage + "damage !!! HP:" + this.hp + "/" + this.maxHp);
+        if (this.hp == 0) {
+            System.out.println(this.name + "was defeated YEAAHH!!!!");
+        }
+    }
+    public void attack(Player player, int damage) {
+        System.out.println(this.name+ "attacks" + player.name + " dealing "+ damage + "DMG!!!!");
+        player.takeDamage(damage);
+
+    }
+    public boolean isAlive() {
+        return this.hp >0;}
 }
+
--- Frontend_RifanoHanifSwandono/core/src/main/java/com/Rifano/frontend/Player.java
+++ Frontend_RifanoHanifSwandono/core/src/main/java/com/Rifano/frontend/Player.java
@@ -12,4 +12,28 @@ public class Player {
         this.power = power;
         this.spellCards = spellCards;
     }
-}
+
+    public void takeDamage(int damage) {
+        this.hp -= damage;
+        if (this.hp < 0) {
+            this.hp = 0;
+        }
+
+        if (this.hp > 0) {
+            System.out.println(this.name + "took" + damage + "damage!!!  Remaining HP:" + this.hp);
+        } else {
+            System.out.println(this.name + "DIEEDDDD BOOOOO");
+        }
+    }
+    public void shoot(Enemy target) {
+        int damage = this.power +10;
+        System.out.println(this.name+"shoots" + target.name+ "dealing"+ damage + "DMG!!!");
+        target.takeDamage(damage);
+    }
+    public boolean isAlive() {
+        return this.hp >0;}
+    }
+
+
+
+
