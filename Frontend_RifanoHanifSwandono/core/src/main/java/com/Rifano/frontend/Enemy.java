3a3162b450ea287cd7ce3410420773ae98031234
Rifanosss <uncalledfornow@gmail.com>
2026-09-04 09:58:04 +0700

    Save local files before pull

---
+++ Frontend_RifanoHanifSwandono/core/src/main/java/com/Rifano/frontend/Enemy.java
@@ -0,0 +1,13 @@
+package com.Rifano.frontend;
+
+public class Enemy {
+    public String name;
+    public int hp;
+    public int maxHp;
+
+    public Enemy(String name, int hp) {
+        this.name = name;
+        this.hp = hp;
+        this.maxHp = hp;
+    }
+}
---
+++ Frontend_RifanoHanifSwandono/core/src/main/java/com/Rifano/frontend/Player.java
@@ -0,0 +1,15 @@
+package com.Rifano.frontend;
+
+public class Player {
+    public String name;
+    public int hp;
+    public int power;
+    public int spellCards;
+
+    public Player(String name, int hp, int power, int spellCards) {
+        this.name = name;
+        this.hp = hp;
+        this.power = power;
+        this.spellCards = spellCards;
+    }
+}
