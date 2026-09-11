445801a4a343c02ff24bb41a264fbc9277d5a905
Rifanosss <uncalledfornow@gmail.com>
2026-09-11 10:40:13 +0700

    RifanoHanifSwandono -init CS2-The thingy thing get get set set

--- Frontend_RifanoHanifSwandono/core/src/main/java/com/Rifano/frontend/Enemy.java
+++ Frontend_RifanoHanifSwandono/core/src/main/java/com/Rifano/frontend/Enemy.java
@@ -1,31 +1,72 @@
 package com.Rifano.frontend;
 
-public class Enemy {
-    public String name;
-    public int hp;
-    public int maxHp;
+import com.badlogic.gdx.graphics.Color;
+
+public class Enemy extends GameObject {
+    protected String name;
+    protected int hp;
+    protected int maxHp;
+    protected long scoreValue;
 
     public Enemy(String name, int hp) {
+        super(200, 380, 24, 24, 0, Color.PINK);
         this.name = name;
         this.hp = hp;
         this.maxHp = hp;
+        this.scoreValue = 100;
     }
-    public void takeDamage(int damage) {
-        this.hp -= damage;
-        if (this.hp < 0) {
-            this.hp = 0;
+
+    public Enemy(float x, float y, float width, float height, Color color, String name, int hp, long scoreValue) {
+        super(x, y, width, height, 0, color);
+        this.name = name;
+        this.hp = hp;
+        this.maxHp = hp;
+        this.scoreValue = scoreValue;
+    }
+
+    public boolean takeDamage(int damage) {
+        if (this.hp <= 0) {
+            return false;
         }
-        System.out.println(this.name + " took" + damage + "damage !!! HP:" + this.hp + "/" + this.maxHp);
+        setHp(getHp() - damage);
+
+        System.out.println(this.name + " took " + damage + " damage !!! HP: " + this.hp + "/" + this.maxHp);
+
         if (this.hp == 0) {
-            System.out.println(this.name + "was defeated YEAAHH!!!!");
+            System.out.println(this.name + " was defeated YEAAHH!!!!");
+            return true;
         }
+        return false;
     }
+
     public void attack(Player player, int damage) {
-        System.out.println(this.name+ "attacks" + player.name + " dealing "+ damage + "DMG!!!!");
+        System.out.println(this.name + "attacks" + player.name + " dealing " + damage + "DMG!!!!");
         player.takeDamage(damage);
 
     }
-    public boolean isAlive() {
-        return this.hp >0;}
-}
 
+    public boolean isAlive() {
+        return this.hp > 0;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getHp() {
+        return hp;
+    }
+    public void setHp(int hp) {
+        this.hp = Math.max(0, hp);
+    }
+    public int getMaxHp() {
+        return maxHp;
+    }
+    public long getScoreValue() {
+        return scoreValue;
+    }
+    public void setScoreValue(long scoreValue) {
+        this.scoreValue = scoreValue;
+    }
+}
--- Frontend_RifanoHanifSwandono/core/src/main/java/com/Rifano/frontend/Player.java
+++ Frontend_RifanoHanifSwandono/core/src/main/java/com/Rifano/frontend/Player.java
@@ -1,39 +1,87 @@
 package com.Rifano.frontend;
 
-public class Player {
-    public String name;
-    public int hp;
-    public int power;
-    public int spellCards;
+import com.badlogic.gdx.graphics.Color;
+
+public class Player extends GameObject {
+    private String name;
+    private int hp;
+    private int power;
+    private int spellCards;
+    private long score;
 
     public Player(String name, int hp, int power, int spellCards) {
+        super(280, 40, 32, 32, 0, Color.RED);
         this.name = name;
         this.hp = hp;
         this.power = power;
         this.spellCards = spellCards;
+        this.score = 0;
+    }
+
+    public Player(float x, float y, String name, int hp, int power, int spellCards) {
+        super(x, y, 32, 32, 0, Color.RED);
+        this.name = name;
+        this.hp = hp;
+        this.power = power;
+        this.spellCards = spellCards;
+        this.score = 0;
     }
 
     public void takeDamage(int damage) {
-        this.hp -= damage;
-        if (this.hp < 0) {
-            this.hp = 0;
-        }
+        setHp(getHp() - damage);
 
         if (this.hp > 0) {
-            System.out.println(this.name + "took" + damage + "damage!!!  Remaining HP:" + this.hp);
+            System.out.println(this.name + " took " + damage + " damage!!!  Remaining HP:" + this.hp);
         } else {
-            System.out.println(this.name + "DIEEDDDD BOOOOO");
+            System.out.println(this.name + " DIEEDDDD BOOOOO ");
         }
     }
+
     public void shoot(Enemy target) {
-        int damage = this.power +10;
-        System.out.println(this.name+"shoots" + target.name+ "dealing"+ damage + "DMG!!!");
+        int damage = 10 + getPower();
+        System.out.println(getName() + " shoots " + target.getName() + " dealing " + damage + " DMG!");
         target.takeDamage(damage);
     }
-    public boolean isAlive() {
-        return this.hp >0;}
+
+    public void addScore(long points) {
+        if (points > 0) {
+            this.score += points;
+        }
     }
 
+    public boolean isAlive() {
+        return this.hp > 0;
+    }
+    public int getHp() {
+        return hp;
+    }
+    public void setHp(int hp) {
+        this.hp = Math.max(0, hp);
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public int getPower() {
+        return power;
+    }
+    public void setPower(int power) {
+        this.power = power;
+    }
 
+    public int getSpellCards() {
+        return spellCards;
+    }
+
+    public void setSpellCards(int spellCards) {
+        this.spellCards = spellCards;
+    }
+
+    public long getScore() {
+        return score;
+    }
+}
 
