a1f476d531bcb20430fd375bef78d54f0f402319
Rifanosss <uncalledfornow@gmail.com>
2026-09-11 10:44:46 +0700

    RifanoHanifSwandono -init CS2-The thingy thing get get set set

---
+++ Frontend_RifanoHanifSwandono/core/src/main/java/com/Rifano/frontend/Boss.java
@@ -0,0 +1,18 @@
+package com.Rifano.frontend;
+
+import com.badlogic.gdx.graphics.Color;
+
+public class Boss extends Enemy {
+
+    public Boss(String name, int hp) {
+        super(380, 400, 48, 48, Color.BLUE, name, hp, 5000L);
+    }
+    public Boss(float x, float y, String name, int hp) {
+        super(x, y, 48, 48, Color.BLUE, name, hp, 5000L);
+    }
+}
+
+//Multilevel Inheritance because Boss inherits from Enemy,
+//which in turn inherits from GameObject.Combined with multiple subclasses Fairy n Boss from Enemy.
+
+
